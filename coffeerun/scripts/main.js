0a3cc14d03ae041e2ab45a7f17442a89a8929c97
Ying <ywen1306@csu.fullerton.edu>
2020-04-15 16:35:29 -0700

    Update main.js

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -2,12 +2,12 @@
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
   var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-  var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
   
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var RemoteDataStore = App.RemoteDataStore;
+  
   var FormHandler = App.FormHandler;
   var Validation = App.Validation;
   var CheckList = App.CheckList;
